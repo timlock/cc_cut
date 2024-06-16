4bf89e12c6d5ec4e2e87e711aecb8d1b34e8889d
timlock <tim-lock@hotmail.de>
2024-06-16 15:10:11 +0200

    flagset accepts all types which implement FromStr

--- src/flags.rs
+++ src/flags.rs
@@ -1,41 +1,55 @@
-use std::
-    collections::HashMap
-;
+use std::collections::HashMap;
+use std::fmt;
+use std::fmt::format;
+use std::str;
 
-pub struct FlagSet<'a> {
-    inner: HashMap<String, &'a mut String>,
+pub struct FlagSet<'a, V> {
+    inner: HashMap<String, &'a mut V>,
 }
 
-impl<'a> FlagSet<'a> {
+impl<'a, V> FlagSet<'a, V>
+where
+    V: str::FromStr,
+{
     pub fn new() -> Self {
         Self {
             inner: HashMap::new(),
         }
     }
 
-    pub fn register(&mut self, flag: String, value: &'a mut String) {
+    pub fn register(&mut self, flag: String, value: &'a mut V) {
         self.inner.insert(flag, value);
     }
 
-    pub fn parse(&mut self, args: impl IntoIterator<Item = String>) -> Vec<String> {
+    pub fn parse(&mut self, args: impl IntoIterator<Item = String>) -> Result<Vec<String>, String>
+    where
+        <V as std::str::FromStr>::Err: std::fmt::Debug,
+    {
         let mut remaining = Vec::new();
         let mut flag = None;
 
         for arg in args {
-            if arg.starts_with('-'){
+            if arg.starts_with('-') {
                 flag = arg.strip_prefix('-').map(|s| s.to_owned());
-            }else{
+            } else {
                 if let Some(key) = flag {
                     if let Some(value) = self.inner.get_mut(&key) {
-                        **value = arg;
+                        **value = match V::from_str(&arg) {
+                            Ok(v) => v,
+                            Err(err) => {
+                                return Err(format!(
+                                    "Could not parse flag {key:?} value {arg}: {err:?}"
+                                ))
+                            }
+                        };
+                        // **value = arg;
                     }
                 }
                 flag = None;
-
             }
         }
 
-        remaining
+        Ok(remaining)
     }
 }
 
@@ -51,7 +65,33 @@ mod tests {
         flagset.register(String::from("key"), &mut value);
 
         let args = vec!["-key".to_string(), "value".to_string()];
-        flagset.parse(args);
+        assert!(flagset.parse(args).is_ok());
         assert_eq!(value.as_str(), "value");
+    }
+
+    #[test]
+    fn test_parse_i32() {
+        struct TestCase {
+            args: Vec<String>,
+            expected_flag: (&'static str, i32),
+            expects_err: bool,
+        }
+        let tests = vec![TestCase {
+            args: vec![String::from("-i"), String::from("1")],
+            expected_flag: ("i", 1),
+            expects_err: false,
+        }];
+
+        for test in tests {
+            let mut flag_set = FlagSet::new();
+
+            let mut value = 0;
+            flag_set.register(test.expected_flag.0.to_string(), &mut value);
+
+            let result = flag_set.parse(test.args);
+            assert_eq!(test.expects_err, result.is_err());
+
+            assert_eq!(test.expected_flag.1, value);
+        }
     }
 }
